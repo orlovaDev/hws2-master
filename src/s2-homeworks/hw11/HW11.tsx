c0aaca3f521a3692bbf2fca5e20504ab270ba4f5
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-08-26 02:00:18 +0300

    solutionHws2-master-hw12(1)

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import {restoreState, saveState} from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
